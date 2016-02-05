a9ddaca567d8a11235758861e4390ac97ecc210c
Mihai Bivol <mm.bivol@gmail.com>
2016-02-05 23:40:26 +0200

    Fix material css

--- webapp/Gruntfile.js
+++ webapp/Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
       },
       builder: {
         files: ['frontend/**/*.js'],
-        tasks: ['browserify'],
+        tasks: ['browserify', 'concat_css'],
       }
     },
     browserify: {
@@ -23,12 +23,19 @@ module.exports = function(grunt) {
             },
         }
     },
+    concat_css: {
+        all: {
+            src: ['node_modules/angular-material/angular-material.min.css'],
+            dest: 'public/bundle.css'
+        }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-express-server');
+  grunt.loadNpmTasks('grunt-concat-css');
 
   grunt.registerTask('default', ['jshint', 'browserify']);
 };
