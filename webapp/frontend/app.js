c8ce6e652a455d1025f6ef2382d8d29752535e18
Catalin Badea <catalin.badea392@gmail.com>
2016-02-05 23:54:17 +0200

    Add basic logic for messages

--- webapp/frontend/app.js
+++ webapp/frontend/app.js
@@ -1,5 +1,67 @@
 (function() {
     var angular = require('angular');
-    var app = angular.module('app', [require('angular-material')]);
-    alert('Yoohooo!');
+    require('angular-animate');
+    require('angular-aria');
+    require('angular-material');
+    var app = angular.module('app', ['ngMaterial']);
+    app.controller('appcontroller', AppController);
+
+    var setError = function(response) {
+        console.log("Encountered error");
+        this.error = response.data.errror;
+    }
+
+    function AppController($scope, $http) {
+        var self = this;
+        $scope.ctrl = this;
+
+        this.$http = $http;
+
+        this.message = "hello world";
+        this.distances = null;
+        this.position = null;
+        this.messages = null;
+        this.newMessage = null;
+
+        navigator.geolocation.getCurrentPosition(function(position) {
+            self.position = position;
+            self.loadMessages();
+        });
+
+        $http.get('/api/post').then(function(response) {
+            console.log(response);
+            self.distances = response.data.distances;
+        }, setError.bind(this));
+    }
+
+    AppController.prototype.isLoaded = function() {
+        return this.position != null && this.distances != null
+               && this.messages != null;
+    }
+
+    AppController.prototype.submitMessage = function() {
+
+        var payload = {
+            message: this.newMessage,
+            location: {
+                longitude: this.position.coords.longitude,
+                latitude: this.position.coords.latitude,
+            }
+        };
+        this.$http.post('/api/post', payload,
+            {headers: {'content-type':'application/json'}}).catch(setError.bind(this));
+        console.log("submit");
+        console.log(payload);
+    };
+
+    AppController.prototype.loadMessages = function () {
+        var self = this;
+        var params = '?longitude=' + this.position.coords.longitude +
+                     '&latitude=' + this.position.coords.latitude;
+
+        this.$http.get('/api/post' + params).then(function(response) {
+            console.log(response.data);
+            self.messages = response.data;
+        }, setError.bind(this));
+    };
 })();
